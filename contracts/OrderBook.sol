85e62b400af2f8f63188770959ae7f08515ff071
KramPiotr <piotrek@krambit.pl>
2024-03-16 07:00:36 +0000

    Implement proper dark pool with the number of orders as the only limiting factor

--- contracts/OrderBook.sol
+++ contracts/OrderBook.sol
@@ -62,15 +62,56 @@ contract OrderBook is IOrderBook, ReentrancyGuard {
      * @notice Place buy order.
      */
     function placeBuyOrder(
+        inEuint256 calldata orderId,
         inEuint256 calldata price,
-        inEuint256 calldata amountOfBaseToken
+        inEuint256 calldata qty
     ) external override nonReentrant { //TODO: we need to make sure that there is a function to check whether your order is still in the order book
         baseToken.transferFromEncrypted(
             msg.sender,
             address(this),
-            amountOfBaseToken
-        );
-        euint256 qtyLeft = FHE.asEuint256(amountOfBaseToken);
+            qty
+        ); //Order: price, qty, 
+        euint256 qtyLeft = FHE.asEuint256(qty);
+        for (uint16 orderIdx = 0; orderIdx < nOrders; orderIdx++) {
+            ebool isCrossing = price.gte(sellBook[orderIdx].price);
+            euint256 potentialQtyFilled = FHE.min(qtyLeft, sellBook[orderIdx].qty);
+            euint256 qtyFilled = FHE.select(isCrossing, potentialQtyFilled, CONST_0_ENCRYPTED);
+            qtyLeft = qtyLeft - qtyFilled;
+
+            results[sellBook[orderIdx].id] = qtyFilled; //TODO: instead of doing this you could just transfer the funds
+        }
+
+        ebool shift = FHE.asEbool(false);
+        euint256 idCarriedOn = orderId;
+        euint256 priceCarriedOn = price;
+        euint256 qtyCarriedOn = qtyLeft;
+        ebool doInsert = qtyLeft.gt(CONST_0_ENCRYPTED);
+        euint256 prevPrice = CONST_0_ENCRYPTED;
+        for (uint16 orderIdx = 0; orderIdx < nOrders; orderIdx++) {
+
+            ebool isCorrectPosition = FHE.and(FHE.eq(prevPrice, price), buyBook[orderIdx].price.gt(price));
+            shift = FHE.or(shift, FHE.and(doInsert, isCorrectPosition));
+            prevPrice = buyBook[orderIdx].price;
+
+            euint256 nextIdCarriedOn = FHE.select(shift, buyBook[orderIdx].id, idCarriedOn);
+            euint256 nextPriceCarriedOn = FHE.select(shift, buyBook[orderIdx].price, priceCarriedOn);
+            euint256 nextQtyCarriedOn = FHE.select(shift, buyBook[orderIdx].qty, qtyCarriedOn);
+
+            buyBook[orderIdx].id = FHE.select(shift, idCarriedOn, buyBook[orderIdx].id);
+            buyBook[orderIdx].price = FHE.select(shift, priceCarriedOn, buyBook[orderIdx].price);
+            buyBook[orderIdx].qty = FHE.select(shift, qtyCarriedOn, buyBook[orderIdx].qty);
+
+            idCarriedOn = nextIdCarriedOn;
+            priceCarriedOn = nextPriceCarriedOn;
+            qtyCarriedOn = nextQtyCarriedOn;
+        }
+        return results;
+    }
+
+
+
+
+
         mapping(euint256 => euint256) results;
         for (uint8 priceLevelIdx = 0; priceLevelIdx < nPriceLevels; priceLevelIdx++) {
             for (uint8 depthIdx = 0; depthIdx < priceLevelDepth; depthIdx++) {
